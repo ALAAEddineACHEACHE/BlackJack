37749192ab11f80eda0b1d6c98e78bc1112db43a
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-02-22 17:01:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 let blackJackGame = {
-'you': {'scoreSpan': '#your-blackjack-result', 'div' : '#your-box' ,'score' :0 },
-'dealer':{'scoreSpan' : '#dealer-blackjack-result' ,'div': '#dealer-box','score':0},
-'cardsM':{'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'11':11,'12':12}
-};  
+    'you': { 'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0 },
+    'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
+    'cards': { '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, '11': 11, '12': 12 }
+};
 const YOU = blackJackGame['you'];
 const DEALER = blackJackGame['dealer'];
-const hitSound = new Audio('assets/sounds/1.mp3');
-const removeSound = new Audio('assets/sounds/2.mp3'); 
-document.querySelector("#blackjack-hit-button").addEventListener('click',blackJackHit);
-document.querySelector("#blackjack-deal-button").addEventListener('click',blackJackDeal);
+const hitSound = new Audio('assets/sounds/swish.mp3');
+const removeSound = new Audio('assets/sounds/Retrieve.mp3');
+document.querySelector("#blackjack-hit-button").addEventListener('click', blackJackHit);
+document.querySelector("#blackjack-stand-button").addEventListener('click',DealerLogic);      
+document.querySelector("#blackjack-deal-button").addEventListener('click', blackJackDeal);
 //User's Shot
-function blackJackHit(){
+function blackJackHit() {
     let card = randomCard();
     console.log(card);
-    showCard(card,YOU);
-    updateScore(card,YOU);
-    showScore(YOU,DEALER);
+    showCard(card, YOU);
+    updateScore(card, YOU);
+    showScore(YOU);
+    
 }
-function randomCard(){
-    let randomIndex = Math.floor(Math.random()*13);
-    return blackJackGame['cardsM'][randomIndex];
+function randomCard() {
+    let randomIndex = Math.floor(Math.random() * 13);
+    return blackJackGame['cards'][randomIndex];
 }
 
-function showCard(card,activePlayer){
+function showCard(card, activePlayer) {
     let cardImage = document.createElement('img');
     cardImage.src = `assets/images/${card}.jpg`;
     hitSound.play();
@@ -30,26 +32,72 @@ function showCard(card,activePlayer){
 
 }
 //Machine's Shot
-function blackJackDeal(){
+function blackJackDeal() {
+    computeWinner();
     let yourImages = document.querySelector('#your-box').querySelectorAll('img');
     let dealerImages = document.querySelector("#dealer-box").querySelectorAll('img');
-    for(i=0;i<yourImages.length;i++){
+    for (i = 0; i < yourImages.length; i++) {
         yourImages[i].remove();
         removeSound.play();
     }
-    for(i=0;i<dealerImages.length;i++){
+    for (i = 0; i < dealerImages.length; i++) {
         dealerImages[i].remove();
         removeSound.play();
     }
+    YOU['score']=0;
+    DEALER['score']=0;
+    document.querySelector("#your-blackjack-result").textContent=0;
+    document.querySelector("#dealer-blackjack-result").textContent=0;
+    document.querySelector("#your-blackjack-result").style.color='#ffffff';
+    document.querySelector("#dealer-blackjack-result").style.color='#ffffff';
 }
-function updateScore(card,activePlayer) {
-    activePlayer['score']+=blackJackGame['cards'];
-    
+function updateScore(card, activePlayer) {
+    activePlayer['score'] += blackJackGame['cards'][card];
+
 }
 function showScore(activePlayer) {
-    document.querySelector(activePlayer['scoreSpan']).textContent=activePlayer['score'];
+    if(activePlayer['score']>=20){
+        document.querySelector(activePlayer['scoreSpan']).textContent="BUST!";
+        document.querySelector(activePlayer['scoreSpan']).style.color="red"
+    }else{
+
+    document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
+}
+}         
+function DealerLogic(){
+    let card = randomCard();   
+    showCard(card,DEALER);
+    updateScore(card,DEALER);
+    showScore(DEALER);
+
+}
+//Decide who's the winner and return who won
+function computeWinner(){
+    let winner;
+    if(YOU['score']<=20){
+//Condition: Higher Score then dealer or if Dealer Busts You will win !
+        if(YOU['score']>DEALER['score'] || DEALER['score']>20){
+            console.log('You win');
+            winner = YOU;
+        }else if(YOU['score']<DEALER['score']){
+            console.log('You lost !');
+            winner = DEALER;
+        }else if(YOU['score']===DEALER['score']){
+            console.log('You Drew');
+        }
+//Condition2:When user busts but dealer doesn't
+
+        }else if(YOU['score']>20 && DEALER['score']<=20){
+        console.log('You lost');
+        winner = DEALER;
+//Condition 3 : When both dealer and user busts 
+
+        }else if(YOU['score']>20 && DEALER['score']>20){
+        console.log('You Drew');
+    }
+        console.log('Winner is '+ winner);
+        return winner;
 }
 
 
 
-    
